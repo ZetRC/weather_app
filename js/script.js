b3953a9927df2eb08e572ceb78c014abad5c675d
Zet Rosas <zetrosas555@gmail.com>
2020-10-12 02:18:39 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+let button = document.querySelector(".button");
+
+
+
+let api_key = "bbc23d45962e19ae83177f0741e24882";
+let buttonOne=document.querySelector(".buttonOne")
+let inputFiled=document.querySelector(".inputField")
+let generalWeather=document.querySelector(".generalWeather")
+let specificWeather=document.querySelector(".specificWeather")
+let place=document.querySelector(".place")
+let weahterIcon=document.querySelector(".weatherIcon")
+let cityArray=["tokyo,japan","berlin,germany","sydney,australia","france,paris"]
+
+buttonOne.addEventListener("click", function () {
+getWeatherData(inputFiled.value)
+inputLogic()
+});
+
+addEventListener("DOMContentLoaded",function(){
+  getWeatherData(cityArray[Math.floor(Math.random()*4)])
+  inputLogic()
+})
+
+
+let getWeatherData=function(value){
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      /* inputFiled.value */value +
+      "&appid=" +
+      api_key
+  )
+    .then((res) => res.json())
+    .then((data) =>{console.log(data)      
+      console.log(data.weather[0].main)
+      console.log(data.weather[0].description)
+      place.innerHTML=data.name
+      let generalWeatherVar=data.weather[0].main
+      if(generalWeatherVar==="Clear"){
+        weahterIcon.innerHTML='<i class="fas fa-sun"></i>'
+      }else if(generalWeatherVar==="Clouds"){
+        weahterIcon.innerHTML='<i class="fas fa-cloud"></i>'
+      }else if(generalWeatherVar==="Rain"){
+        weahterIcon.innerHTML='<i class="fas fa-tint"></i>'
+      }else{
+        weahterIcon.innerHTML='<i class="fas fa-cloud"></i>'
+      }
+      generalWeather.innerHTML=data.weather[0].main+"<br>"
+      specificWeather.innerHTML=data.weather[0].description
+    })
+}
+/* 
+let weahterIcon=document.querySelector(".weatherIcon")
+let generalWeatherVar=generalWeather.innerHTML
+console.log(generalWeatherVar)
+let getIcon=function(){
+  if(generalWeatherVar==="Clea"){
+    weahterIcon.innerHTML='<i class="fas fa-sun"></i>'
+  }else if(generalWeatherVar=="Clouds"){
+    weahterIcon='<i class="fas fa-cloud"></i>'
+  }else if(generalWeatherVar=="Rain"){
+    weahterIcon='<i class="fas fa-cloud-showers-heavy"></i>'
+  }else{
+    console.log("err")
+  }
+
+  console.log(weahterIcon.innerHTML)
+}
+ */
+let inputLogic=function(){
+  console.log(inputFiled.value)
+  inputFiled.value=""
+}
+
+
+
+
