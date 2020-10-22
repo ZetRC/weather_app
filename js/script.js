52c00cef267ff31d43d21f5b3fe38ea78d723430
Zet Rosas <zetrosas555@gmail.com>
2020-10-22 18:38:26 -0500

    backup

--- js/script.js
+++ js/script.js
@@ -12,23 +12,25 @@ let weahterIcon=document.querySelector(".weatherIcon")
 let cityArray=["tokyo,japan","berlin,germany","sydney,australia","paris,france"]
 let temperature=document.querySelector(".temperature")
 let country=document.querySelector(".country")
-
+let inputalue=inputFiled.value
 buttonOne.addEventListener("click", function () {
 getWeatherData(inputFiled.value)
+getWeeklyWeather(inputFiled.value)
+console.log(inputFiled.value)
 inputLogic()
 });
 
 addEventListener("DOMContentLoaded",function(){
   getWeatherData(cityArray[Math.floor(Math.random()*4)])
   inputLogic()
-  getWeeklyWeather()
+  
 })
 let dayTwoWeather=document.querySelector(".dayTwoWeather")
 let dayThreeWeather=document.querySelector(".dayThreeWeather")
 let dayFourWeather=document.querySelector(".dayFourWeather")
 let dayFiveWeather=document.querySelector(".dayFiveWeather")
-let getWeeklyWeather=function(){
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=london,uk&appid="+api_key)
+let getWeeklyWeather=function(inputValue){
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q="+inputValue+"&appid="+api_key)
   .then(res=>res.json())
   .then(data=>{
     console.log(data)
@@ -39,6 +41,9 @@ let getWeeklyWeather=function(){
     console.log(data.list[0].dt_txt)
     console.log(data.list[0].weather[0].main)
     console.log(data.list[0].weather[0].description)
+    console.log(data.list[8])
+    console.log(data.list[16])
+    console.log(data.list[24])
     
     let getIcon=function(weatherVar){
       if(weatherVar==="Clear"){
@@ -62,9 +67,9 @@ let getWeeklyWeather=function(){
  */
     
 dayTwoWeather.innerHTML="<h1>"+getIcon(data.list[8].weather[0].main)+"</h1><h3>"+data.list[8].weather[0].main+"</h3><h5>"+data.list[8].dt_txt.substr(0,10)+"</h5>"
-dayThreeWeather.innerHTML="<h1>"+getIcon(data.list[16].weather[0].main)+"</h1><h3>"+data.list[16].weather[0].main+"</h3><h5>"+data.list[8].dt_txt.substr(0,10)+"</h5>"
-dayFourWeather.innerHTML="<h1>"+getIcon(data.list[24].weather[0].main)+"</h1><h3>"+data.list[24].weather[0].main+"</h3><h5>"+data.list[8].dt_txt.substr(0,10)+"</h5>"
-dayFiveWeather.innerHTML="<h1>"+getIcon(data.list[32].weather[0].main)+"</h1><h3>"+data.list[32].weather[0].main+"</h3><h5>"+data.list[8].dt_txt.substr(0,10)+"</h5>"
+dayThreeWeather.innerHTML="<h1>"+getIcon(data.list[16].weather[0].main)+"</h1><h3>"+data.list[16].weather[0].main+"</h3><h5>"+data.list[16].dt_txt.substr(0,10)+"</h5>"
+dayFourWeather.innerHTML="<h1>"+getIcon(data.list[24].weather[0].main)+"</h1><h3>"+data.list[24].weather[0].main+"</h3><h5>"+data.list[24].dt_txt.substr(0,10)+"</h5>"
+dayFiveWeather.innerHTML="<h1>"+getIcon(data.list[32].weather[0].main)+"</h1><h3>"+data.list[32].weather[0].main+"</h3><h5>"+data.list[32].dt_txt.substr(0,10)+"</h5>"
 })
 }
 
