f37bee59ed22a7cf9bec52227ed4c894525f03f8
Zet Rosas <zetrosas555@gmail.com>
2020-10-22 19:20:10 -0500

    backup

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ let inputalue=inputFiled.value
 buttonOne.addEventListener("click", function () {
 getWeatherData(inputFiled.value)
 getWeeklyWeather(inputFiled.value)
-console.log(inputFiled.value)
 inputLogic()
 });
 
@@ -34,16 +33,17 @@ let getWeeklyWeather=function(inputValue){
   .then(res=>res.json())
   .then(data=>{
     console.log(data)
-    console.log(data.list)
-    console.log(data.list[0])
-    console.log(data.list[0].weather)
-    console.log(data.list[0].weather[0])
+    console.log(data.city.name)
+    console.log(data.city.country)
+    console.log(data.list[0].main) 
+    console.log(data.list[0].main.temp)/* 
+    console.log(data.list[0].main.temp-max)
+    console.log(data.list[0].main.temp-min) */
     console.log(data.list[0].dt_txt)
-    console.log(data.list[0].weather[0].main)
-    console.log(data.list[0].weather[0].description)
-    console.log(data.list[8])
-    console.log(data.list[16])
-    console.log(data.list[24])
+    console.log(data.list[8].weather[0].main)
+    console.log(data.list[8].weather[0].description)
+   
+
     
     let getIcon=function(weatherVar){
       if(weatherVar==="Clear"){
@@ -57,15 +57,7 @@ let getWeeklyWeather=function(inputValue){
       }
     }
 
-    console.log(getIcon("Clear"))
-/* 
-    let addTwoNumbers=function(a,b){
-      return a+b
-    }
 
-    console.log(addTwoNumbers(1,2))
- */
-    
 dayTwoWeather.innerHTML="<h1>"+getIcon(data.list[8].weather[0].main)+"</h1><h3>"+data.list[8].weather[0].main+"</h3><h5>"+data.list[8].dt_txt.substr(0,10)+"</h5>"
 dayThreeWeather.innerHTML="<h1>"+getIcon(data.list[16].weather[0].main)+"</h1><h3>"+data.list[16].weather[0].main+"</h3><h5>"+data.list[16].dt_txt.substr(0,10)+"</h5>"
 dayFourWeather.innerHTML="<h1>"+getIcon(data.list[24].weather[0].main)+"</h1><h3>"+data.list[24].weather[0].main+"</h3><h5>"+data.list[24].dt_txt.substr(0,10)+"</h5>"
