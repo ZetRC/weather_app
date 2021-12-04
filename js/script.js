e205d2dd5f55d0a9ff13644259b6ae7d00fe82ca
ZetRC <zetrosas555@gmail.com>
2021-12-04 12:02:48 -0600

    changes in layout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-let button = document.querySelector(".button");
-
-
-
+/* let button = document.querySelector(".button"); */
 let api_key = "bbc23d45962e19ae83177f0741e24882";
 let buttonOne=document.querySelector(".buttonOne")
 let inputFiled=document.querySelector(".inputField")
-let generalWeather=document.querySelector(".generalWeather")
+//
 let specificWeather=document.querySelector(".specificWeather")
 let place=document.querySelector(".place")
 let weahterIcon=document.querySelector(".weatherIcon")
@@ -41,13 +38,7 @@ let getWeeklyWeather=function(inputValue){
   .then(res=>res.json())
   .then(data=>{
     console.log(data)
-    console.log(data.city.name)
-    console.log(data.city.country)
-    console.log(data.list[0].main.temp)
-    console.log(data.list[0].dt_txt)
-    console.log(data.list[0].weather[0].main)
-    console.log(data.list[0].weather[0].description)    
-    
+
     let getIcon=function(weatherVar){
       if(weatherVar==="Clear"){
         return '<i class="fas fa-sun"></i>'
@@ -60,21 +51,32 @@ let getWeeklyWeather=function(inputValue){
       }
     }
 
+    /* main box data */
 
     place.innerHTML=data.city.name+","
     country.innerHTML=data.city.country
     time.innerHTML=data.list[0].dt_txt.substr(0,10)
     temperature.innerHTML=Math.floor(data.list[0].main.temp-273.15)+"C"
     weahterIcon.innerHTML= getIcon(data.list[0].weather[0].main)
-    generalWeather.innerHTML=data.list[0].weather[0].main
-      specificWeather.innerHTML=data.list[0].weather[0].description
+    specificWeather.innerHTML=data.list[0].weather[0].description
 
-    let getWeeklyWaetherData
+    /* weekly wheater box data */
 
-dayTwoWeather.innerHTML="<div>"+getIcon(data.list[8].weather[0].main)+"</div>"+Math.floor(data.list[8].main.temp-273.15)+"C"+"</h6>"+"</div><h5>"+data.list[8].dt_txt.substr(5,6)+"</h5>"
-dayThreeWeather.innerHTML="<div>"+getIcon(data.list[16].weather[0].main)+"</div>"+Math.floor(data.list[16].main.temp-273.15)+"C"+"</div><h5>"+data.list[16].dt_txt.substr(5,6)+"</h5>"
-dayFourWeather.innerHTML="<div>"+getIcon(data.list[24].weather[0].main)+"</div>"+Math.floor(data.list[24].main.temp-273.15)+"C"+"</div><h5>"+data.list[24].dt_txt.substr(5,6)+"</h5>"
-dayFiveWeather.innerHTML="<div>"+getIcon(data.list[32].weather[0].main)+"</div>"+Math.floor(data.list[32].main.temp-273.15)+"C"+"</div><h5>"+data.list[32].dt_txt.substr(5,6)+"</h5>"
+    dayTwoWeather.innerHTML="<div>"+getIcon(data.list[8].weather[0].main)+"</div>"+Math.floor(data.list[8].main.temp-273.15)+"C"+"</h6>"+"</div><h5>"+data.list[8].dt_txt.substr(5,6)+"</h5>"
+    dayThreeWeather.innerHTML="<div>"+getIcon(data.list[16].weather[0].main)+"</div>"+Math.floor(data.list[16].main.temp-273.15)+"C"+"</div><h5>"+data.list[16].dt_txt.substr(5,6)+"</h5>"
+    dayFourWeather.innerHTML="<div>"+getIcon(data.list[24].weather[0].main)+"</div>"+Math.floor(data.list[24].main.temp-273.15)+"C"+"</div><h5>"+data.list[24].dt_txt.substr(5,6)+"</h5>"
+    dayFiveWeather.innerHTML="<div>"+getIcon(data.list[32].weather[0].main)+"</div>"+Math.floor(data.list[32].main.temp-273.15)+"C"+"</div><h5>"+data.list[32].dt_txt.substr(5,6)+"</h5>"
+
+    /* other data box */
+
+    document.querySelector(".population").innerHTML=data.city.population
+    document.querySelector(".latitude").innerHTML=data.city.coord.lat
+    document.querySelector(".longitude").innerHTML=data.city.coord.lon
+    document.querySelector(".timezone").innerHTML=data.city.timezone
+    document.querySelector(".sunrise").innerHTML=data.city.sunrise
+    document.querySelector(".sunset").innerHTML=data.city.sunset
+
+
 })
 }
 
@@ -84,12 +86,3 @@ let inputLogic=function(){
 }
 
 weekWeatherContainer=document.querySelector(".weekWeatherContainer")
-
-
-
-$(function(){
-$(".slidebarIcon").on("click",function(){
-  $(".weekWeatherContainer").animate({width:'toggle'},500);
-  
-})
-})
